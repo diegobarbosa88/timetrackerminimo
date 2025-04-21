150bb9abdbe86e3c94f9b052bfd6450021907343
diegobarbosa88 <88diegobarbosa@gmail.com>
2025-04-21 09:53:41 +0200

    Create page.tsx

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+<nav>
+  <ul className="flex space-x-6">
+    <li><a href="/" className="hover:underline">Inicio</a></li>
+    <li><a href="/dashboard" className="hover:underline">Dashboard</a></li>
+    <li><a href="/admin/employees" className="hover:underline">Empleados</a></li>
+    <li><a href="/reports" className="hover:underline">Informes</a></li>
+  </ul>
+</nav>
